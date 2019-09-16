e023f0cdcb453aa6b717ded9879dd340bb51d041
Luis Dominguez <reynaldodominguez@gmail.com>
2019-09-16 11:01:13 -0400

    HomeWork third fase

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,10 @@
 var friendsData = require("../data/friends"); 
 
 module.exports = function(app) {
-    // API GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-    // ---------------------------------------------------------------------------
-  
     app.get("/api/friends", function(req, res) {
       res.json(friendsData);
     });
+    
   //  app.post("/api/tables", function(req, res) {
       // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
       // It will do this by sending out the value "true" have a table
@@ -23,10 +18,6 @@ module.exports = function(app) {
       //  res.json(false);
      // }
     };
-  
-    // ---------------------------------------------------------------------------
-    // I added this below code so you could clear out the table while working with the functionality.
-    // Don"t worry about it!
-  
+ 
 
   
