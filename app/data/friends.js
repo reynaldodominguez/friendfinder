728e123356dae4fe601f54c7ad9a3e9961102253
Luis Dominguez <reynaldodominguez@gmail.com>
2019-09-17 20:52:19 -0400

    HomeWork Final Version

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendArray = [
     {
-        name: "Luis",
-        photo: "none",
+        name: "Jennifer",
+        photo: "https://www.mujer.com.pa/sites/default/files/field/image/jennifer-lopez-2019-tour-pr-billboard-1548.jpg",
         scores: [
             "5",
             "3",
@@ -16,8 +16,8 @@ var friendArray = [
         ]
     },
     {
-        name: "Maria",
-        photo: "none",
+        name: "Miley",
+        photo: "https://www.diariogol.com/uploads/s1/66/72/28/7/miley-cyrus_15_970x597.jpeg",
         scores: [
             "1",
             "3",
@@ -33,8 +33,8 @@ var friendArray = [
     }
     ,
     {
-        name: "Guillermo",
-        photo: "none",
+        name: "Dwayne",
+        photo: "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5c7bb396160b52c67730cc1b107de07f%2F400x400.jpg",
         scores: [
             "2",
             "3",
@@ -49,8 +49,8 @@ var friendArray = [
         ]
     },
     {
-        name: "Richard",
-        photo: "none",
+        name: "John",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/8/80/Elton_John_2011_Shankbone_2_%28cropped%29.JPG",
         scores: [
             "5",
             "5",
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,21 +14,21 @@ module.exports = function (app) {
     var matchImage = "";
     var totalDiff = 10000;
 
-    for (var i = 0; i < friendsdata.length; i++) {
+    for (var i = 0; i < friendsData.length; i++) {
       var diff = 0;
 
       for (var j = 0; j < userResponses.length; j++) {
-        diff += Math.abs(friendsdata[i].scores[j] - userResponses[j]);
+        diff += Math.abs(friendsData[i].scores[j] - userResponses[j]);
       }
 
       if (diff < totalDiff) {
         totalDiff = diff;
-        matchName = friendsdata[i].name;
-        matchImage = friendsdata[i].photo;
+        matchName = friendsData[i].name;
+        matchImage = friendsData[i].photo;
       }
 
     }
-    friends.push(userInfo);
+    friendsData.push(userInfo);
 
     res.json({ status: "OK", matchName: matchName, matchImage: matchImage });
   });
